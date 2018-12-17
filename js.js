1997647d1ff7c6f7d1ee0f404def01138a9f0ba9
benjaminmcmillan <benmcmillan@fastmail.co.uk>
2018-12-17 15:22:00 +0100

    updated js and added 2 more descriptions

---
+++ js.js
@@ -0,0 +1,103 @@
+function display(x) {
+    var text = document.getElementById("IoT");
+    var text2 = document.querySelector("#secondInfo");
+    text2.style.opacity = "0.2"
+    console.log(text2)
+    text.style.display = "block";
+}
+
+function displayOff(x) {
+  var text = document.getElementById("IoT");
+  text.style.display = "none";
+  var text2 = document.querySelector("#secondInfo");
+  text2.style.opacity = "1"
+}
+
+function display2(x) {
+    var text = document.getElementById("OtBT");
+    var text2 = document.querySelector("#secondInfo");
+    text2.style.opacity = "0.2"
+    console.log(text2)
+    text.style.display = "block";
+}
+
+function displayOff2(x) {
+  var text = document.getElementById("OtBT");
+  text.style.display = "none";
+  var text2 = document.querySelector("#secondInfo");
+  text2.style.opacity = "1"
+}
+
+function display3(x) {
+    var text = document.getElementById("AoV");
+    var text2 = document.querySelector("#secondInfo");
+    text2.style.opacity = "0.2"
+    console.log(text2)
+    text.style.display = "block";
+}
+
+function displayOff3(x) {
+  var text = document.getElementById("AoV");
+  text.style.display = "none";
+  var text2 = document.querySelector("#secondInfo");
+  text2.style.opacity = "1"
+}
+
+window.onload = () => {
+
+
+var getAcmeTag = document.querySelector('#workOne');
+var acmeImages = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
+
+var i = 0;
+var l = acmeImages.length - 1;
+
+(function loop() {
+  if (i > l) {
+    i = 0;
+  }
+  getAcmeTag.src = "work/IoT/" + acmeImages[i];
+  loopTimer = setTimeout(loop, 3000);
+  ++i;
+})();
+
+
+var getOtBTTag = document.querySelector('#workFour');
+var OtBTImages = ["1.jpeg", "2.jpeg"];
+
+var ii = 0;
+var ll = OtBTImages.length - 1;
+
+(function loop() {
+  if (ii > ll) {
+    ii = 0;
+  }
+  getOtBTTag.src = "work/OtBT/" + OtBTImages[ii];
+  loopTimer = setTimeout(loop, 3000);
+  ++ii;
+})();
+
+
+
+
+var getDiv = document.querySelector("#workThree")
+
+getDiv.onlick = function changeThis() {
+var getAoVTag = document.querySelector('#workThree');
+var AoVImages = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg"];
+
+var iii = 0;
+var lll = AoVImages.length - 1;
+
+(function loop() {
+  if (iii > lll) {
+    iii = 0;
+  }
+  getAoVTag.src = "work/AoV/" + AoVImages[iii];
+  // loopTimer = setTimeout(loop, 3000);
+
+  ++iii;
+})();
+}
+
+}
